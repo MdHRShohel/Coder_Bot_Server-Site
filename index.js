683fd833a21dc739abbe682dd47c0abdf861939f
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-25 18:21:04 +0600

    course categories added

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port =process.env.PORT || 5000;
 
+const categories = require('./data/CourseCategories.json');
 
 app.get('/',(req,res)=>{
     res.send('CODER BOT API RUNNING...');
+});
+
+app.get('/course-categories',(req,res)=>{
+    res.json(categories);
 });
 
 app.listen(port,()=>{
