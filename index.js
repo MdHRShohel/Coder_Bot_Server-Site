d8197e60c3b132e10e2e59f84a0d0346eea43151
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-26 23:47:10 +0600

    single course added

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const port =process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/CourseCategories.json');
+const courses = require('./data/Courses.json');
 
 app.get('/',(req,res)=>{
     res.send('CODER BOT API RUNNING...');
 });
 
-app.get('/course-categories',(req,res)=>{
-    res.json(categories);
+app.get('/courses',(req,res)=>{
+    res.json(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((c) => c.id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port,()=>{
